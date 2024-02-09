22781e164f3abc221d05f8059abed89af7923ceb
Mariia Cherenkova <thejastin@gmail.com>
2024-02-09 14:47:17 -0800

    Rock, Paper, Scissors v3

--- PycharmProjects/pythonProject_ADA/main.py
+++ PycharmProjects/pythonProject_ADA/main.py
@@ -1,13 +1,39 @@
-def total_even_numbers(num):
-    sum = 0
-    for n in range(0,num+1,2):
-        sum += n
-    return sum
+import random
 
-assert total_even_numbers(6) == 12, f"Reported {total_even_numbers(6)} for 'total_even_numbers(6)' instead of 12"
-assert total_even_numbers(0) == 0, f"Reported {total_even_numbers(6)} for 'total_even_numbers(6)' instead of 0"
-assert total_even_numbers(1) == 0, f"Reported {total_even_numbers(6)} for 'total_even_numbers(6)' instead of 0"
-assert total_even_numbers(15) == 56, f"Reported {total_even_numbers(6)} for 'total_even_numbers(6)' instead of 56"
+def choose_rps():
+    return random.choice(["rock", "paper", "scissors"])
+    # r = random.randint(0,2)
+    # if r == 0:
+    #     return "rock"
+    # elif r == 1:
+    #     return "paper"
+    # else:
+    #     return "scissors"
 
-num = 9
-print(total_even_numbers(num))
+#print(choose_rps())
+
+def RPS_game(player1, player2):
+    if player1 == player2:
+        print("It's a TIE!")
+    elif player1 == "rock":
+        if player2 == "paper":
+            print("Player2 WIN!")
+        else:
+            print("Player1 WIN!")
+    elif player1 == "paper":
+        if player2 == "rock":
+            print("Player1 WIN!")
+        else:
+            print("Player2 WIN!")
+    elif player1 == "scissors":
+        if player2 == "paper":
+            print("Player1 WIN!")
+        else:
+            print("Player2 WIN!")
+
+player1 = choose_rps()
+print(player1)
+player2 = choose_rps()
+print(player2)
+
+RPS_game(player1, player2)
