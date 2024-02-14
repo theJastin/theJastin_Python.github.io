324f072ecdb1dc31d7756c6b8a4d062273995711
Mariia Cherenkova <thejastin@gmail.com>
2024-02-13 21:39:45 -0800

    Account Generator v2

--- PycharmProjects/AccountGenerator/main.py
+++ PycharmProjects/AccountGenerator/main.py
@@ -1,22 +1,55 @@
 import random
 
 # Step1 - list of student names
+# student_accounts = [
+#     {
+#         "name" : "ROSSIE MARTINEZ",
+#         "id" : "100000",
+#         "email" : "RMARTINEZ@adadevelopersacademy.org"
+#     },
+#     {
+#         "name" : "JOHN SMITH",
+#         "id" : "100001",
+#         "email": "JSMITH@adadevelopersacademy.org"
+#     },
+#     {
+#         "name" : "ANNA BOLD",
+#         "id" : "100002",
+#         "email" : "ABOLD@adadevelopersacademy.org"
+#     },
+#     {
+#         "name": "JULIA RANCH",
+#         "id" : "100003",
+#         "email" : "JRANCH@adadevelopersacademy.org"
+#     },
+#     {
+#         "name" : "DELIA AGHO",
+#         "id" : "100004",
+#         "email" : "DAGHO@adadevelopersacademy.org"
+#     }
+# ]
 student_names = ["ROSSIE MARTINEZ", "JOHN SMITH", "ANNA BOLD", "JULIA RANCH", "DELIA AGHO"]
 
+student_accounts = []
 # Step2 - list of student IDs
-student_ids = []
-for i in range(5): # for name in student_names
-    student_ids.append(random.randint(111111, 999999))
-
 # Step3 - list of emails (student names, IDs)
-student_emails = []
 for i in range(5):
-    student_name = student_names[i]
-    [first, last] = student_name.split(" ")
+    student = {}
+    student["name"] = student_names[i]
+    student["id"] = random.randint(111111, 999999)
 
-    student_id = str(student_ids[i])
-    student_emails.append(first[0] + last + str(student_id[-3:]) + "@example.org")
+    [first, last] = student_names[i].split(" ")
+    student["email"] = first[0] + last + str(student["id"])[-3:] + "@example.org"
+
+    print(f'name: {student["name"]}\nid: {student["id"]}\nemail: {student["email"]}\n')
+
+    student_accounts.append(student)
 
 # Print each student account information
-for i in range(5):
-    print(f"name: {student_names[i]}\nid: {student_ids[i]}\nemail: {student_emails[i]}\n")
+# for account in student_accounts:
+#     for key in account:
+#         print(f'{key}: {account[key]}')
+#     print()
+
+# for account in student_accounts:
+#     print(f'name: {account.get("name")}\nid:{account.get("id")}\nemail: {account.get("email")}\n')
