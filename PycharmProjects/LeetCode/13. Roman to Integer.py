8614fae3b57c2b8e74d0106d02609a1f0caa7128
Mariia Cherenkova <thejastin@gmail.com>
2024-02-14 19:26:55 -0800

    13. Roman to Integer

--- PycharmProjects/LeetCode/13. Roman to Integer.py
+++ PycharmProjects/LeetCode/13. Roman to Integer.py
@@ -1,56 +1,19 @@
-# Symbol       Value
-# I             1
-# V             5
-# X             10
-# L             50
-# C             100
-# D             500
-# M             1000
 def romanToInt(s):
-    """
-    :type s: str
-    :rtype: int
-    """
-    dictionary = {
-        "I":"1",
-        "V":"5",
-        "X":"10",
-        "L":"50",
-        "C":"100",
-        "D":"500",
-        "M":"1000"
-    }
-
-    result = 0
+    roman = {"I":1,"IV":4,"V":5,"IX":9,"X":10,"XL":40,"L":50,"XC":90,
+             "C":100,"CD":400,"D":500,"CM":900,"M":1000}
+    integer = 0
     i = 0
     while i < len(s):
         if i != len(s)-1:
-            if s[i] == "I" and (s[i+1] == "V" or s[i+1] == "X"):
-                if s[i+1] == "V":
-                    result += 4
-                elif s[i+1] == "X":
-                    result += 9
-                i+=2
-            elif s[i] == "X" and (s[i+1] == "L" or s[i+1] == "C"):
-                if s[i+1] == "L":
-                    result += 40
-                elif s[i+1] == "C":
-                    result += 90
-                i+= 2
-            elif s[i] == "C" and (s[i+1] == "D" or s[i+1] == "M"):
-                if s[i+1] == "D":
-                    result += 400
-                elif s[i+1] == "M":
-                    result += 900
-                i+=2
+            if roman.get(s[i:i+2]) != None:
+                integer += roman.get(s[i:i+2])
+                i += 2
             else:
-                result += int(dictionary[s[i]])
-                i+=1
+                integer += roman[s[i]]
+                i += 1
         else:
-            result += int(dictionary[s[i]])
-    return result
-# I can be placed before V (5) and X (10) to make 4 and 9.
-# X can be placed before L (50) and C (100) to make 40 and 90.
-# C can be placed before D (500) and M (1000) to make 400 and 900.
+            integer += roman[s[i]]
+            i += 1
+    return integer
 
 print(romanToInt("IIIX"))
