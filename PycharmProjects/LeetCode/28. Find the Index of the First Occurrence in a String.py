26cf69e72f4247b4309ddc96b72dbb6bbc35f626
Mariia Cherenkova <thejastin@gmail.com>
2024-02-16 14:35:05 -0800

    28. Find the Index of the First Occurrence in a String v2

--- PycharmProjects/LeetCode/28. Find the Index of the First Occurrence in a String.py
+++ PycharmProjects/LeetCode/28. Find the Index of the First Occurrence in a String.py
@@ -1,8 +1,13 @@
 def strStr(haystack, needle):
-    return haystack.find(needle)
-
+    needle_len = len(needle)
+    for i in range(len(haystack)):
+        if haystack[i:i+needle_len] == needle:
+            return i
+    return -1
 # haystack = "sadbutsad"
 # needle = "sad" # output = 0
-haystack = "leetcode"
-needle = "leeto" # output = -1
+# haystack = "leetcode"
+# needle = "leeto" # output = -1
+haystack = "a"
+needle = "a"
 print(strStr(haystack, needle))
