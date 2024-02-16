edbce0850f17f07dc64b9e7aaa5c3ed82b69ce78
Mariia Cherenkova <thejastin@gmail.com>
2024-02-16 14:12:30 -0800

    27. Remove Element v2

--- PycharmProjects/LeetCode/27. Remove Element.py
+++ PycharmProjects/LeetCode/27. Remove Element.py
@@ -1,13 +1,12 @@
 def removeElement(nums, val):
-    j = 0
+    k = 0
     for i in range(len(nums)):
         if nums[i] != val:
-            nums[j] = nums[i]
-            j += 1
+            nums[k] = nums[i]
+            k += 1
         else:
             nums[i] = None
-    k = j
-    return nums
+    return k
 
 # Input: nums = [3,2,2,3], val = 3
 # Output: 2, nums = [2,2,_,_]
