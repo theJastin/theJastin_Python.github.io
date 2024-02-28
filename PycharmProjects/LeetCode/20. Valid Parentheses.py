89f670f35a46773353287bbcfb8f3f4d6cec9a35
Mariia Cherenkova <145719758+theJastin@users.noreply.github.com>
2024-02-28 12:41:25 -0800

    Update 20. Valid Parentheses.py

--- PycharmProjects/LeetCode/20. Valid Parentheses.py
+++ PycharmProjects/LeetCode/20. Valid Parentheses.py
@@ -1,3 +1,4 @@
+# 20. Valid Parentheses solution
 def isValid(s):
     # dictionary = {"(":0, ")":0, "[":0, "]":0, "{":0, "}":0}
     list = []
@@ -16,3 +17,27 @@ def isValid(s):
 
 s = "([]){}"
 print(isValid(s))
+
+# problem
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+Example 1:
+Input: s = "()"
+Output: true
+
+Example 2:
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+Input: s = "(]"
+Output: false
+ 
+Constraints:
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
