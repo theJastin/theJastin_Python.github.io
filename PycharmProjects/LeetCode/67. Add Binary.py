88db06e2e1035065649ed3a69ba90e8d8018e8d5
Mariia Cherenkova <145719758+theJastin@users.noreply.github.com>
2024-02-28 13:02:15 -0800

    Update 67. Add Binary.py

--- PycharmProjects/LeetCode/67. Add Binary.py
+++ PycharmProjects/LeetCode/67. Add Binary.py
@@ -1,3 +1,4 @@
+# 67. Add Binary solution
 # 0 + 0 = 0, carry 0
 # 1 + 0 = 1, carry 0
 # 0 + 1 = 1, carry 0
@@ -44,3 +45,21 @@ def addBinary(a, b):
 a = "1010"
 b = "1011"
 print(addBinary(a, b))
+
+# problem
+"""
+Given two binary strings a and b, return their sum as a binary string.
+
+Example 1:
+Input: a = "11", b = "1"
+Output: "100"
+
+Example 2:
+Input: a = "1010", b = "1011"
+Output: "10101"
+
+Constraints:
+1 <= a.length, b.length <= 104
+a and b consist only of '0' or '1' characters.
+Each string does not contain leading zeros except for the zero itself.
+"""
