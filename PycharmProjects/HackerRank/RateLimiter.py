c1fa37b6b70de9a1fb2187881ab1508945381fdf
Mariia Cherenkova <thejastin@gmail.com>
2024-03-04 14:31:28 -0800

    RateLimiter v2, Sliding Window

--- PycharmProjects/HackerRank/RateLimiter.py
+++ PycharmProjects/HackerRank/RateLimiter.py
@@ -8,32 +8,23 @@ request_list1 = ["URL1", "URL2", "URL1", "URL2", "URL3", "URL1", "URL1", "URL8",
 # window = 5
 def Verify(request_list, i, limit, window):
     URL = request_list[i]
-    tmp_limit = 0
-    if i < window:
-        start = 0
-        end = 0
-    else:
+    url_limit = 0
+    if i >= limit:
         start = i-1
-        end = i-window
-    # going backwards in window
-    for j in range(start, end, -1):
-        if request_list[j] == URL:
-            tmp_limit += 1
-    if tmp_limit >= limit:
-        return False
-    else:
-        return True
+        end = max(0, i-window)-1
+        # going backwards in window
+        for j in range(start, end, -1):
+            if request_list[j] == URL:
+                url_limit += 1
+    return url_limit < limit
 
 def RateLimiter(request_list):
     result = []
     for i in range(len(request_list)):
-        # isGood = verify 1
-        # if not isGood return "bad"
-        # isGood = verify 2
         log = "good"
         if not Verify(request_list, i, 2, 5) or not Verify(request_list, i, 5, 30):
             log = "bad"
         result.append(log)
     return result
 
-print(RateLimiter(request_list))
+print(RateLimiter(request_list1))
