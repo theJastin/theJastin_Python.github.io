9cd4a32c9b459ad22ecd087b8d0413d6ce363d7d
Mariia Cherenkova <thejastin@gmail.com>
2024-03-04 14:56:30 -0800

    Sliding Window v2

--- PycharmProjects/HackerRank/Sliding Window.py
+++ PycharmProjects/HackerRank/Sliding Window.py
@@ -1,12 +1,10 @@
 # Max number of vowels in a substring
 def slidingWindow(s, k):
-    vowel_list = ['a', 'e', 'i', 'o', 'u']
-    substring = s[:k]
+    vowel_list = {'a', 'e', 'i', 'o', 'u'} #hashset
     total_vowels = 0
-    for char in substring:
-        if char in vowel_list:
-            total_vowels += 1
-    max_vowels = max(0, total_vowels)
+    for i in range(k):
+        total_vowels += 1 if s[i] in vowel_list else 0
+    max_vowels = total_vowels
     for i in range(len(s)-k):
         if s[i] in vowel_list:
             total_vowels -= 1
