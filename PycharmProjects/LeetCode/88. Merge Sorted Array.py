5c7307c00b1830f0bbf729ef79973d6d61aac745
Mariia Cherenkova <thejastin@gmail.com>
2024-03-13 13:50:45 -0700

    88. Merge Sorted Array, v1

--- PycharmProjects/LeetCode/88. Merge Sorted Array.py
+++ PycharmProjects/LeetCode/88. Merge Sorted Array.py
@@ -1,13 +1,45 @@
-# 88. Merge Sorted Array
-# :type nums1: List[int]
-# :type m: int
-# :type nums2: List[int]
-# :type n: int
-# :rtype: None
+#88. Merge Sorted Array solution
 # Do not return anything, modify nums1 in -place instead.
-"""
+def merge(nums1, m, nums2, n):
+    p1 = m-1
+    p2 = n-1
+    for p in range(m+n-1, -1, -1):
+        choose = p1 >= 0 and (p2 < 0 or nums2[p2] <= nums1[p1])
+        if choose:
+            nums1[p] = nums1[p1]
+            p1 -= 1
+        else:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+    return nums1
 
-def merge(self, nums1, m, nums2, n):
+# nums1 = [1,2,3,0,0,0]
+# m = 3
+# nums2 = [2,5,6]
+# n = 3
+#Output: [1, 2, 2, 3, 5, 6]
+
+# nums1 = [1]
+# m = 1
+# nums2 = []
+# n = 0
+
+# nums1 = [0]
+# m = 0
+# nums2 = [1]
+# n = 1
+# Output: [1]
+
+nums1 = [4,5,6,0,0,0]
+m = 3
+nums2 = [1,2,3]
+n = 3
+
+# print(nums1)
+# print(nums2)
+print(merge(nums1, m, nums2, n))
+
+# problem
 """
 You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n,
 representing the number of elements in nums1 and nums2 respectively.
